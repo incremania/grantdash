eb1c89ddc9b72a1e941efc1c6f4fa180c983d957
incremania <106455212+incremania@users.noreply.github.com>
2023-07-07 16:54:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const sectionEl = document.querySelector('section')
 
 //show users
 const usersAdmin = async () => {
-let res = await fetch('https://grantb.onrender.com/user/all/admin/error', {
+let res = await fetch('https://grantb.onrender.com/user/all/admin/', {
       method: 'GET',
       
 })
